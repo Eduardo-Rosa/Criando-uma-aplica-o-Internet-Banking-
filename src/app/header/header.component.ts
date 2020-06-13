0f3146826038b2d8d98599a2ff5f985a0b44097b
Eduardo-Rosa <edu.betim@aol.com>
2020-06-13 09:44:37 -0300

    ajustes nas imagens e algumas rotas

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,25 +1,65 @@
-// aqui estamos importando o component do pacote angular core
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-//  Aqui decoramos a classe do component App Header
+
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ModalNotCadastroComponent } from '../modal-not-cadastro/modal-not-cadastro.component';
+import { SingletonRouterService } from 'src/services/singletonRouter.service';
+import { MatDialog } from '@angular/material/dialog';
+import { OnInit, Component } from '@angular/core';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-// Aqui exportamos a classe HeaderComponent
 export class HeaderComponent implements OnInit {
-  formLogin
-  // aqui definimos o constructor da aplicacao
-  constructor(private fb: FormBuilder) { }
+  formLogin;
+  theEvent;
+  key;
+  regex;
+  keys;
+  getCadastro;
+  message;
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    public dialog: MatDialog) { }
 
-  
   ngOnInit() {
-    console.log('Component app-header inciado...');
     this.formLogin = this.fb.group({
       cpf: ['']
     });
-    
+  }
+  // regexp
+  onlynumber(evt) {
+    this.theEvent = evt || window.event;
+    this.key = this.theEvent.keyCode || this.theEvent.which;
+    this.key = String.fromCharCode(this.key);
+    this.regex = /^[0-9.]+$/;
+    if (!this.regex.test(this.key)) {
+      this.theEvent.returnValue = false;
+      if (this.theEvent.preventDefault) {
+        this.theEvent.preventDefault();
+      }
+    }
+  }
+  login() {
+    this.getCadastro = JSON.parse(localStorage.getItem('cadastro'));
+    const cpfPersistido = this.getCadastro['cpf'];
+    const cpfDigitado = this.formLogin.get('cpf').value;
+    if (cpfPersistido === cpfDigitado) {
+      this.router.navigate(['home-logada']);
+    } else {
+      this.openDialog();
+    }
   }
 
+  openDialog() {
+    const dialogRef = this.dialog.open(ModalNotCadastroComponent, {
+      height: '350px'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 }
